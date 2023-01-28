08df9180dbb969454d641e6fabfcaa01b1060691
YourUserName <YourEmailAddress>
2023-01-29 00:54:04 +0300

    commit

--- sql.py
+++ sql.py
@@ -1,10 +1,6 @@
 import sqlite3
 con = sqlite3.connect("vpn.db")
 cur = con.cursor()
-cur.execute("create table plateji(uid,id_plateja,summa)")
-con.commit()
-cur.execute("create table all_info(uid,keys,key_id,periods)")
-con.commit()
 cur.execute('select * from all_info')
 print(cur.fetchall())
 cur.execute('select * from plateji')
