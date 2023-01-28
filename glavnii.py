fd4df04db36129c73a525ce0b0e6f203d32fe66f
YourUserName <YourEmailAddress>
2023-01-28 22:43:58 +0300

    commit

---
+++ glavnii.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+import time
+
+client = subprocess.Popen([sys.executable, 'main.py'])
+time.sleep(10)
+
+client2 = subprocess.Popen([sys.executable, 'waller.py'])
+
+client.wait()
