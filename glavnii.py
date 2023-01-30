53d36d8e632e61451af3de4cf7ec6393cb43503d
YourUserName <YourEmailAddress>
2023-01-30 06:11:50 +0300

    commit

--- glavnii.py
+++ glavnii.py
@@ -6,5 +6,6 @@ client = subprocess.Popen([sys.executable, 'main.py'])
 time.sleep(10)
 
 client2 = subprocess.Popen([sys.executable, 'waller.py'])
-
+time.sleep(5)
+client3 = subprocess.Popen([sys.executable, 'time_manager.py'])
 client.wait()
