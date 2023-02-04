73b3b46ac7d88b4bfbe24a55fe59e346df1648a9
YourUserName <YourEmailAddress>
2023-02-04 21:11:10 +0300

    commit

---
+++ chek_bd.py
@@ -0,0 +1,23 @@
+import sqlite3
+con = sqlite3.connect("vpn.db")
+cur = con.cursor()
+cur.execute('select * from users')
+print('\n\n\n\nusers:')
+for i in cur.fetchall():
+    print(i)
+cur.execute('select * from referal')
+print('\n\n\n\nreferal:')
+for i in cur.fetchall():
+    print(i)
+cur.execute('select * from all_referals')
+print('\n\n\n\nall referals:')
+for i in cur.fetchall():
+    print(i)
+cur.execute('select * from History_payments')
+print('\n\n\n\nHistory_payments:')
+for i in cur.fetchall():
+    print(i)
+cur.execute('select * from active_payments')
+print('\n\n\n\nactive_payments:')
+for i in cur.fetchall():
+    print(i)
--- sql.py
+++ sql.py
@@ -1,6 +1,8 @@
 import sqlite3
 con = sqlite3.connect("vpn.db")
 cur = con.cursor()
+cur.execute("create table all_referals(referal_nickname varchar(50),percent_referal integer,kolichestvo_platejei integer , user_percent)")
+con.commit()
 cur.execute('create table referal (uid integer,referal_nickname varchar(50),percent_referal integer,kolichestvo_platejei integer , user_percent)')
 con.commit()
 cur.execute('create table users(uid integer,keys varchar(50),key_id integer,for_date varchar(50))')
