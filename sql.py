82a5c5841c48e4571b7164a9f7b6f27e6e5c0799
YourUserName <YourEmailAddress>
2023-02-04 19:51:24 +0300

    commit

--- sql.py
+++ sql.py
@@ -1,22 +1,14 @@
 import sqlite3
 con = sqlite3.connect("vpn.db")
 cur = con.cursor()
-cur.execute('create table referal (uid,ref)')
+cur.execute('create table referal (uid integer,referal_nickname varchar(50),percent_referal integer,kolichestvo_platejei integer , user_percent)')
 con.commit()
-cur.execute('select * from referal')
-print(cur.fetchall())
+cur.execute('create table users(uid integer,keys varchar(50),key_id integer,for_date varchar(50))')
 con.commit()
-cur.execute('select * from all_info')
-print(cur.fetchall())
-cur.execute('select * from plateji')
-print(cur.fetchall())
-cur.execute("create table plateji(uid,id_plateja,summa)")
+cur.execute('create table History_payments (uid integer,referal_nickname varchar(50),payment integer,status integer ,date_of_payment)')
 con.commit()
-cur.execute("create table all_info(uid,keys,key_id,periods)")
+cur.execute("create table active_payments(uid integer,id_plateja varchar(50),for_month varchar(50))")
 con.commit()
-cur.execute('select * from all_info')
-print(cur.fetchall())
-
 
 
 
