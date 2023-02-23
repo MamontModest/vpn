d9ee55cfd4726fd2f8ba5b10ec5fe53a9d6f6654
YourUserName <YourEmailAddress>
2023-02-23 15:40:07 +0300

    1commit

--- glavnii.py
+++ glavnii.py
@@ -5,7 +5,7 @@ import time
 client = subprocess.Popen([sys.executable, 'main.py'])
 time.sleep(10)
 
-client2 = subprocess.Popen([sys.executable, 'waller.py'])
+client2 = subprocess.Popen([sys.executable, 'webhook.py'])
 time.sleep(5)
 client3 = subprocess.Popen([sys.executable, 'time_manager.py'])
 time.sleep(5)
